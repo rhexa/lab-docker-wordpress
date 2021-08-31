b51031134c39e46fe3d186159072ecb2e16dd306
rhexa <akun.kripitit@gmail.com>
2021-08-31 10:52:16 +0300

    added network for db provisioning

--- mysql-client.sh
+++ mysql-client.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# manual client
 docker run -it --network wp-net --rm mysql mysql -h db -u exampleuser -p
+
+# docker compose client
+docker run -it --network docker-wordpress_db-provision --rm mysql mysql -h db -u exampleuser -p
